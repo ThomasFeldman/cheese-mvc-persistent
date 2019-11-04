5a299f03c0c33275033c71d05ba9fd3a56bcb5af
ThomasFeldman <tfeldman1.31@gmail.com>
2019-11-03 21:02:05 -0600

    Part One Done

---
+++ src/main/java/org/launchcode/models/Category.java
@@ -0,0 +1,48 @@
+package org.launchcode.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.JoinColumn;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.*;
+
+@Entity
+public class Category {
+
+
+    @OneToMany
+    @JoinColumn(name = "category_id")
+    private List<Cheese> cheeses = new ArrayList<>();
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @NotNull
+    @Size(min = 3, max = 15)
+    private String name;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Category() {
+    }
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+}
